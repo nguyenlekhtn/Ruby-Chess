76351f75b0d2defe5ca3ebc08f9d7ecdf3277895
nguyenlekhtn <nguyenlekhtn@gmail.com>
2022-05-27 22:00:44 +0700

    add checking arguemt numbers to validatorInterface test

--- spec/validator_interface_spec.rb
+++ spec/validator_interface_spec.rb
@@ -1,5 +1,5 @@
 shared_examples 'has validator interface' do
   it 'responds to valid?' do
-    expect(subject).to respond_to(:valid?)
+    expect(subject).to respond_to(:valid?).with(2).arguments
   end
 end
