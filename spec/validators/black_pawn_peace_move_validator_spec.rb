59545a2413c6063b2a3aeb71ce8a762cf5447002
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2022-06-02 21:53:30 +0700

    fix describe name

--- spec/validators/black_pawn_peace_move_validator_spec.rb
+++ spec/validators/black_pawn_peace_move_validator_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../../lib/library'
 require_relative '../validator_interface_spec'
 
-describe  do
+describe BlackPawnPeaceMoveValidator do
   let(:board) { instance_double(Board) }
   subject(:validator) { described_class.new(board) }
 
