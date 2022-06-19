8a31a336277e22f3e45b9df36e9d84a24feb01ad
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2022-06-19 09:56:10 +0700

    move  tests from Game#checkmated? to  CheckMateChecker

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -63,40 +63,4 @@ describe Game do
       end
     end
   end
-
-  describe '#checkmated?' do
-    subject(:game) { described_class.new(board: board, color: color) }
-
-
-    context 'when black is checkmated, ' do
-      let(:color) { Color::BLACK }
-      let(:board) { Board.for('rnbqkbnr/ppppp2p/8/5ppQ/4P3/2N5/PPPP1PPP/R1B1KBNR') }
-
-      it 'returns true' do
-        result = game.checkmated?
-        expect(result).to be true
-      end
-    end
-
-    context 'when white is not checkmated' do
-      let(:color) { Color::WHITE }
-      let(:board) { Board.for('1Q6/5pk1/2p3p1/1p2N2p/1b5P/1bn5/r5P1/1K6') }
-
-
-      it 'returns false' do
-        result = game.checkmated?
-        expect(result).to be false
-      end
-    end
-
-    context 'when white is checkmated' do\
-      let(:color) { Color::WHITE }
-      let(:board) { Board.for('1Q6/5pk1/2p3p1/1p2N2p/1b5P/1bn5/2r3P1/2K5') }
-
-      it 'returns true' do
-        result = game.checkmated?
-        expect(result).to be true
-      end
-    end
-  end
 end
