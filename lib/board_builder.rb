302dd7fd71acbe10e51ca6a40b50473560beba96
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2022-06-19 17:22:25 +0700

    remove board dependency from BoardBuilder

--- lib/board_builder.rb
+++ lib/board_builder.rb
@@ -1,24 +1,18 @@
 # frozen_string_literal: true
 
 class BoardBuilder
-  attr_reader :board
-
-  def initialize(board)
-    @board = board
-  end
-
   def cell(string)
     if %w[r n b q k p].include? string.downcase
-      Piece.for(string, board)
+      Piece.for(string)
     elsif ('1'..'8').include?(string)
-      Array.new(string.to_i) { Piece.for('', board) }
+      Array.new(string.to_i) { Piece.for('') }
     else
       throw 'Invalid FEN notation'
     end
   end
 
   def row(string)
-    return Array.new(8) { Piece.for('', board) } if string == '8'
+    return Array.new(8) { Piece.for('') } if string == '8'
 
     string.chars.map { |char| cell(char) }.flatten
   end
