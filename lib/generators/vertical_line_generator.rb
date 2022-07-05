238b12f3f251c6b607a775f40485c6ac75ee3f48
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2022-07-05 23:23:40 +0700

    fix VerticlalLIneGenerator not inherets correctly

--- lib/generators/vertical_line_generator.rb
+++ lib/generators/vertical_line_generator.rb
@@ -1,11 +1,11 @@
 require_relative '../moving_in_directions.rb'
 
-class VerticalLineGenerator
+class VerticalLineGenerator < LineGenerator
   def directions
     [[-1, 0], [1, 0]]
   end
 
   def no_piece_between_2_cells?(start_pos, end_pos)
-    board.no_piece_in_horizontal_line_between_2_cells?(start_pos, end_pos)
+    board.no_piece_in_vertical_line_between_2_cells?(start_pos, end_pos)
   end
 end
