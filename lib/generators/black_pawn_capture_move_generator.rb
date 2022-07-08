bc220b29007f35b3b07b4750f1c5858037de0886
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2022-07-08 22:25:44 +0700

    add BlackPawnCaptureMoveGenerator class and its test

---
+++ lib/generators/black_pawn_capture_move_generator.rb
@@ -0,0 +1,16 @@
+class BlackPawnCaptureMoveGenerator < Generator
+
+  def cells(origin)
+    neighbors(origin).filter { |neighbor| valid?(origin, neighbor) }
+  end
+
+  def neighbors(pos)
+    [pos.jump(row_step: -1, col_step: -1), pos.jump(row_step: -1, col_step: 1)].compact
+  end
+
+  def valid?(start_pos, end_pos)
+    return false unless board.opposite_color_between_2_cells?(start_pos, end_pos)
+
+    true
+  end
+end
---
+++ spec/generators/black_pawn_capture_move_generator_spec.rb
@@ -0,0 +1,32 @@
+require_relative '../../lib/library'
+require_relative '../generator_interface_spec'
+
+describe BlackPawnCaptureMoveGenerator do
+  subject(:generator) { described_class.new(board:) }
+
+  context 'when the subject acts as a generator' do
+    let(:board) { Board.for }
+    include_examples 'implement the generator interface'
+  end
+
+  describe '#cells' do
+    context 'if 2 forward diagonal cells both have oppopsite pieces' do
+      let(:board) { Board.for('8/5p2/4B1B1/8/8/8/8/8') }
+      it 'should return them' do  
+        expected = [Cell.for('E6'), Cell.for('G6')]
+        result = generator.cells(Cell.for('F7'))
+        expect(result).to match_array expected
+      end
+    end
+
+    context 'if 2 forward diagonal cells none have oppopsite pieces' do
+      let(:board) { Board.for('8/5p2/8/8/8/8/8/8') }
+      it 'should return empty array' do  
+        expected = []
+        result = generator.cells(Cell.for('F7'))
+        expect(result).to match_array expected
+
+      end
+    end
+  end
+end
