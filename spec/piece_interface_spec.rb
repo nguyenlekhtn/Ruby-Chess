ac1e865cb48be3be84e124a2fc82d48b71f000c6
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2022-07-09 21:10:12 +0700

    add neighbor_generator check
    add 'responds to neighbor_generator' check to piece_interface_spec

--- spec/piece_interface_spec.rb
+++ spec/piece_interface_spec.rb
@@ -6,4 +6,8 @@ shared_examples 'has piece interface' do
   it 'responds to to_s' do
     expect(subject).to respond_to(:to_s)
   end
+
+  it 'responds to neighbor_generator' do
+    expect(subject).to respond_to(:neighbor_generator).with(1).argument
+  end
 end
