022d94837d4f16a02c676ae60ce37e694d2843d9
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2022-07-13 21:55:50 +0700

    add inital test for navigator

---
+++ spec/navigator_spec.rb
@@ -0,0 +1,26 @@
+require_relative '../lib/library'
+
+describe Navigator do
+  describe '#cells' do
+    subject(:navigator) { Navigator.new(board:) }
+
+    context 'given C51 Evans Gambit board' do
+      let(:board) { Board.for('r1bqk1nr/pppp1ppp/2n5/2b1p3/1PB1P3/5N2/P1PP1PPP/RNBQK2R') }
+
+      it 'should return all neighbors from C5' do
+        expected = [
+          Cell.for('F2'),
+          Cell.for('E3'),
+          Cell.for('D4'),
+          Cell.for('F8'),
+          Cell.for('E7'),
+          Cell.for('D6'),
+          Cell.for('B6'),
+          Cell.for('B4'),
+        ]
+        result = navigator.cells(Cell.for('C5'))
+        expect(result).to match_array expected
+      end
+    end
+  end
+end
