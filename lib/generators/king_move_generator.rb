2b67a65698dbb11126753d13bb4357ab07f151a6
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2022-07-16 23:28:11 +0700

    add a filter case for KingMoveGenerator#cells

--- lib/generators/king_move_generator.rb
+++ lib/generators/king_move_generator.rb
@@ -1,6 +1,14 @@
 class KingMoveGenerator < Generator
+  attr_reader :navigator
+
+  def initialize(game)
+    super
+    @navigator = Navigator.new(game)
+  end
+
   def cells(origin)
-    neighbors(origin)
+    color = board.get_color_at(origin)
+    neighbors(origin).reject { |neighbor| be_attackable_by_opposite_player_at_position?(color:, position: neighbor)}
   end
 
   def neighbors(origin)
@@ -16,8 +24,8 @@ class KingMoveGenerator < Generator
     ].map { |row_step, col_step| origin.jump(row_step:, col_step:) }.compact
   end
 
-  def be_attackable_by_opposite_player?(pos)
-
+  def be_attackable_by_opposite_player_at_position?(color:, position:)
+    navigator.legal_moves_of_a_player(color.opposite).include?(position)
   end
 end
 
