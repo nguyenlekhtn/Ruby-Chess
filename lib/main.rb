bb7afcb66302ed894a8fd707a1f3396ec3996885
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2022-07-19 20:27:42 +0700

    move main.rb to root

--- lib/main.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'library'
-
-Game.new.play
-# Game.new(board: Board.for('rnbqkbnr/ppppp2p/8/5ppQ/4P3/2N5/PPPP1PPP/R1B1KBNR'), color: Color::BLACK).play
