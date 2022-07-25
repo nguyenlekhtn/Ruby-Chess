087eb6847fe170e3ead283350d53f7a75540177b
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2022-07-25 22:07:52 +0700

    remove validators

--- lib/validators/diagonal_validator.rb
+++
@@ -1,9 +0,0 @@
-class DiagonalValidator < SegmentValidator
-  def same_line?(start_pos, end_pos)
-    start_pos.same_diagonal_line_with_cell?(end_pos)
-  end
-
-  def no_piece_between_2_cells?(start_pos, end_pos)
-    board.no_piece_in_diagonal_line_between_2_cells?(start_pos, end_pos)
-  end
-end
--- lib/validators/horizontal_validator.rb
+++
@@ -1,9 +0,0 @@
-class HorizontalValidator < SegmentValidator
-  def same_line?(start_pos, end_pos)
-    start_pos.same_horizontal_line_with_cell?(end_pos)
-  end
-
-  def no_piece_between_2_cells?(start_pos, end_pos)
-    board.no_piece_in_horizontal_line_between_2_cells?(start_pos, end_pos)
-  end
-end
