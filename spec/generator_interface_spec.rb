dfbe0db9b76099ef2e2b5da4601703540c227508
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2022-07-31 19:49:09 +0700

    add #moves to generator interface

--- spec/generator_interface_spec.rb
+++ spec/generator_interface_spec.rb
@@ -2,4 +2,8 @@ shared_examples 'implement the generator interface' do
   it 'responds to cells' do
     expect(subject).to respond_to(:cells).with(1).arguments
   end
+
+  it 'responds to moves' do
+    expect(subject).to respond_to(:moves).with(1).arguments
+  end
 end
