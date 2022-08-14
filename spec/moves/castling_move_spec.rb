67fd277130cf649fd685031e367a73148b13bf02
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2022-08-15 06:59:34 +0700

    add CastlingMove test

---
+++ spec/moves/castling_move_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../../lib/library'
+
+describe CastlingMove do
+  describe 'board_after_move' do
+    subject(:move) { described_class.new(KingsideCastlingGenerator.new(game, WhiteSide.new)) }
+    let(:game) { Game.new(board:) }
+
+    context 'when white kingside castling happened' do
+      let(:board) { Board.for('4k3/8/8/8/8/8/8/4K2R')}
+
+      it 'should move king and rook to correct positon' do
+        result = move.board_after_move(board)
+        expected = Board.for('4k3/8/8/8/8/8/8/5RK1')
+        expect(result).to eq expected
+      end
+    end
+  end
+end
