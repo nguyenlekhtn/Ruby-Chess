7c4f17efb10df41066993449009815a03ac0a71a
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2022-08-20 17:50:20 +0700

    Change generator interface
    remove #generate_capturable_move? from generator interface

--- lib/moves/normal_move.rb
+++ lib/moves/normal_move.rb
@@ -4,8 +4,4 @@ module NormalMove
   def generate_moves(origin)
     cells(origin).map { |target| Move.new(origin:, target:) }
   end
-
-  def generate_capturable_move?
-    true
-  end
 end
--- spec/generator_interface_spec.rb
+++ spec/generator_interface_spec.rb
@@ -1,15 +1,7 @@
 # frozen_string_literal: true
 
 shared_examples 'implement the generator interface' do
-  it 'responds to cells' do
-    expect(subject).to respond_to(:cells).with(1).arguments
-  end
-
   it 'responds to generate_moves' do
     expect(subject).to respond_to(:generate_moves).with(1).arguments
-  end
-
-  it 'responds to generate_capturable_move?' do
-    expect(subject).to respond_to(:generate_capturable_move?)
   end
 end
