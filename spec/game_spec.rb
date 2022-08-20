01050d53ea9dd03b586eff3678a38ea83146a1c5
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2022-08-20 10:41:10 +0700

    add test for Game#change_board_by_move

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -14,4 +14,15 @@ describe Game do
       end
     end
   end
+
+  describe '#change_board_by_move' do
+    subject(:game) { described_class.new(board: Board.for) }
+    let(:move) { instance_double(Move) }
+
+    it 'should change board to be same as move.board_after_move' do
+      new_board = Board.for('rnbqkbnr/pppppppp/8/8/8/4P3/PPPP1PPP/RNBQKBNR') 
+      allow(move).to receive(:board_after_move).and_return(new_board)
+      expect { game.change_board_by_move(move) }.to change { game.board }.to(new_board)
+    end
+  end
 end
