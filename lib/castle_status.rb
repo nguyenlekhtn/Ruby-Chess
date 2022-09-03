c01ccff89e9edba09841375eef7da91974a25647
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2022-09-03 12:18:40 +0700

    change inner of CastleStatus

--- lib/castle_status.rb
+++ lib/castle_status.rb
@@ -1,37 +1,51 @@
+# frozen_string_literal: true
+
 class CastleStatus
-  def initialize
-    @is_king_not_moved = { 'black' => true, 'white' => true }
-    @is_rook_not_moved = { 'black' => { queenside: true, kingside: true },
-                           'white' => { queenside: true, kingside: true }
-                         }
+  attr_reader :color, :castle_avail
+
+  def initialize(**opts)
+    @color = opts[:color]
+    @castle_avail = opts[:castle_avail] || { 'Q' => true, 'K' => true }
   end
 
-  def able_to_castle?(color, is_kingside)
-    king_not_moved?(color) && rook_not_moved?(color, is_kingside)
+  def able_to_castle?(is_kingside)
+    king_not_moved? && rook_not_moved?(is_kingside)
+  end
+
+  def able_to_castle_at_side?(is_kingside)
+    @castle_avail[side(is_kingside)]
   end
   
-  def mark_rook_moved(color, is_kingside)
-    @is_rook_not_moved[color.value][side(is_kingside)] = false
+  def mark_rook_moved(is_kingside)
+    @castle_avail[side(is_kingside)] = false
+    CastleStatus.new(color:, castle_avail: { **@castle_avail, side(is_kingside) => false })
   end
 
-  def mark_king_moved(color)
-    @is_king_not_moved[color.value]  = false
+  def mark_king_moved
+    CastleStatus.new(color:, castle_avail: { 'Q' => false, 'K' => false })
   end
 
-  def king_not_moved?(color)
-    @is_king_not_moved[color.value]
-  end
-
-  def rook_not_moved?(color, is_kingside)
-    @is_rook_not_moved[color.value][side(is_kingside)]
-  end
-  
   def side(is_kingside)
-    return :kingside if is_kingside
+    return 'K' if is_kingside
 
-    :queenside
+    'Q'
   end
 
-  private
-  attr_reader :status
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'color'        => @color,
+      'castle_avail' => @castle_avail
+    }.to_json(*args)
+  end
+
+  def self.json_create(object)
+    new(color: object['color'], castle_avail: object['castle_avail'])
+  end
+
+  def eql?(other)
+    other.class == self.class && other.color == @color && other.castle_avail == @castle_avail
+  end
+
+  alias == eql?
 end
--- spec/castle_status_spec.rb
+++ spec/castle_status_spec.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+# frozen_string_lxiteral: true
 
 require_relative '../lib/library'
 
@@ -7,56 +7,90 @@ describe CastleStatus do
 
   describe '#mark_king_moved' do
     context 'when black king is marked moved' do
-      it 'the change should take affect' do
-        expect { castle.mark_king_moved(Color::BLACK) }
-        .to change { castle.king_not_moved?(Color::BLACK) }
-        .from(true)
-        .to(false)
+      subject(:castle_black) { described_class.new(color: Color::BLACK) }
+
+      it 'queenside is not able to castle' do
+        castle_after_king_moved = castle_black.mark_king_moved
+        result = castle_after_king_moved.able_to_castle_at_side?(true)
+        expect(result).to be false
+      end
+
+      it 'kingside is not able to castle' do
+        castle_after_king_moved = castle_black.mark_king_moved
+        result = castle_after_king_moved.able_to_castle_at_side?(false)
+        expect(result).to be false
       end
     end
   end
 
   describe '#mark_rook_moved' do
-    context 'when white right rook is marked moved' do
-      it 'the change should take affect' do
-        expect { castle.mark_rook_moved(Color::WHITE, true) }
-        .to change { castle.rook_not_moved?(Color::WHITE, true) }
-        .from(true)
-        .to(false)
+    context 'when white kingside rook is marked moved' do
+      subject(:castle_white) { described_class.new(color: Color::WHITE) }
+
+      it 'kingside is not able to castle' do
+        castle_after_kingside_rook_moved = castle_white.mark_rook_moved(true)
+        result = castle_after_kingside_rook_moved.able_to_castle_at_side?(true)
+        expect(result).to be false
+      end
+    end
+
+    context 'when WHITE queenside rook is marked moved' do
+      subject(:castle_white) { described_class.new(color: Color::WHITE) }
+
+      it 'queenside is not able to castle' do
+        castle_after_queenside_rook_moved = castle_white.mark_rook_moved(false)
+        result = castle_after_queenside_rook_moved.able_to_castle_at_side?(false)
+        expect(result).to be false
       end
     end
   end
 
-  describe '#able_to_castle?' do
-    context 'when white right rook and king is not moved' do
-      it 'white is to castle kingside' do
-        result = castle.able_to_castle?(Color::WHITE, true)
+  describe '#able_to_castle_at_side?' do
+    context 'when quenside is not able to castle' do
+      subject(:castle_white) { described_class.new(color: Color::WHITE, castle_avail: { 'Q' => false, 'K' => true }) }
+
+      it 'returns false' do
+        result = castle_white.able_to_castle_at_side?(false)
+        expect(result).to be false
+      end
+    end
+
+    context 'when quenside is able to castle' do
+      subject(:castle_white) { described_class.new(color: Color::WHITE, castle_avail: { 'Q' => true, 'K' => true }) }
+
+      it 'returns true' do
+        result = castle_white.able_to_castle_at_side?(false)
         expect(result).to be true
       end
     end
 
-    context 'when white right rook is marked moved' do
-      it 'white is unable to castle kingside' do
-        castle.mark_rook_moved(Color::WHITE, true)
-        result = castle.able_to_castle?(Color::WHITE, true)
+    context 'when kingside is not able to castle' do
+      subject(:castle_white) { described_class.new(color: Color::WHITE, castle_avail: { 'Q' => true, 'K' => false }) }
+
+      it 'returns false' do
+        result = castle_white.able_to_castle_at_side?(true)
         expect(result).to be false
       end
     end
 
-    context 'when black king is marked moved' do
-      before do
-        castle.mark_king_moved(Color::BLACK)
-      end
+    context 'when kignside is able to castle' do
+      subject(:castle_white) { described_class.new(color: Color::WHITE, castle_avail: { 'Q' => true, 'K' => true }) }
 
-      it 'black is unable to castle kingside' do
-        result = castle.able_to_castle?(Color::BLACK, true)
-        expect(result).to be false
+      it 'returns true' do
+        result = castle_white.able_to_castle_at_side?(true)
+        expect(result).to be true
       end
+    end
+  end
 
-      it 'black is unable to castle kingside' do
-        castle.mark_king_moved(Color::BLACK)
-        result = castle.able_to_castle?(Color::BLACK, false)
-        expect(result).to be false
+  describe ':json_create' do
+    context 'when the object is converted to json' do
+      subject(:castle_json) { described_class.new(color: Color::WHITE, castle_avail: { 'Q' => true, 'K' => false }) }
+
+      it 'is able to converted back' do
+        json = JSON.generate(castle_json)
+        result = JSON.parse(json, create_additions: true)
+        expect(result).to eql castle_json
       end
     end
   end
